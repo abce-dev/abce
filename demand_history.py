1e1368a3eb29b5159e0c8a57c6e382bb60211b72
kbiegel <kbiegel@anl.gov>
2020-09-21 11:45:57 -0500

    added docstrings to all methods

--- demand_history.py
+++ demand_history.py
@@ -4,9 +4,20 @@
 import pandas as pd
 
 class DemandHistory(object):
+    """A publicly-available history of current and past demand quantities.
+    """
     def __init__(self, init_data):
-        ''' 
-        '''
+        """Create an instance of the DemandHistory class.
+
+           Parameters
+           ----------
+           init_data : list of floats or ints
+               Prior history of demand which agents can know.
+
+           Returns
+           -------
+           new DemandHistory object
+        """
         if not isinstance(init_data, list):
             init_data = [init_data]
         period = list(range(len(init_data)))
@@ -15,5 +26,18 @@ class DemandHistory(object):
 
 
     def add_data(self, new_demand_point):
+        """Add a new data point to the time series.
+
+           Parameters
+           ----------
+           new_demand_point: int or float
+               A new data point to append to the end of the current data
+               series; most often used to transfer the upcoming "future"
+               demand to the most recent past period.
+
+           Returns
+           -------
+           None
+        """
         self.register = self.register.append({'demand': new_demand_point},
                                              ignore_index=True)
--- id_register.py
+++ id_register.py
@@ -1,20 +1,49 @@
 # A class to hold the register of all generation unit IDs currently in use
 
 class ID_register(object):
+    """A class to provide a public register of all generation unit ID numbers
+       which are currently claimed, and to assign ID numbers to new units.
+    """
     def __init__(self):
+        """Creates an ID_register object, and sets the starting ID number
+           to 100.
+        """
         self.register = list()
         self.next_id = 100
 
     def add_unit(self, agent_id, unit_id):
+        """Add a unit to the register, and change the next available ID.
+        """
         self.register.append(unit_id)
         self.update_next_id()
 
     def update_next_id(self):
-        ''' Update the next available project ID, ensuring no collisions with
-              existing units. If the next ID is less than any entries in
-              the register, reset it to be one greater than the largest
-              ID number currently in the register.
-        '''
+        """Update the next available project ID.
+
+           Detailed Description
+           --------------------
+           Update the next available project ID, ensuring no collisions with
+           existing units.
+
+           If the next ID is less than any entries in the register, reset it
+           to be one greater than the largest ID number currently in the
+           register.
+
+           If the next ID is a valid unclaimed ID, do nothing.
+
+           This function is called by both the add_unit() and the
+           get_next_available_id() functions, so the next available unit ID
+           is always guaranteed to be a valid unclaimed ID after those
+           operations.
+
+           Parameters
+           ----------
+           none
+
+           Returns
+           -------
+           None
+        """
         while self.next_id in self.register:
             self.next_id += 1
         if self.register != []:
@@ -22,6 +51,19 @@ class ID_register(object):
                 self.next_id = max(self.register) + 1
 
     def get_next_available_id(self):
+        """Return the next unclaimed unit ID, and then update the next claimable
+           unit ID number.
+
+           Parameters
+           ----------
+           None
+
+           Returns
+           -------
+           assigned_id : int
+              The next (sequentially) valid unclaimed unit ID number, to be
+              assigned to a new generation unit.
+        """
         assigned_id = self.next_id
         self.update_next_id()
         return assigned_id
