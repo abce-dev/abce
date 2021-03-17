f0f8b3a91f353f5b1c7a2216f05ed504ef1e7d14
biegelk <biegelk3@gmail.com>
2021-03-17 18:18:11 -0500

    turned file into an importable or executable module

--- seed_creator.py
+++ seed_creator.py
@@ -22,37 +22,63 @@
 import sqlite3
 import os
 
-# Set name and path for ABCE database file
-abce_db = "./abce_db.db"
+def set_database_filename():
+    abce_db = "./abce_db.db"
+    return abce_db
 
-# If file already exists, ask if it's OK to delete it
-if os.path.exists(abce_db):
+def clear_db_file(abce_db):
+    if os.path.exists(abce_db):
+        user_resp = get_user_consent_to_delete(abce_db)
+        if user_resp in ["Y", "y", "Yes", "yes", "YES"]:
+            os.remove(abce_db)
+            print(f"Existing file at {abce_db} deleted.")
+        else:
+            print(f"Okay, please remove the file at {abce_db} before running this script again.")
+            print("Terminating...")
+            exit()
+
+def get_user_consent_to_delete(abce_db):
     print(f"A file already exists at {abce_db}.")
     user_resp = ""
     valid_responses = ["Y", "y", "N", "n", "yes", "no", "Yes", "No"]
-    yes = ["Y", "y", "yes", "Yes"]
     while user_resp not in valid_responses:
         user_resp = input("Is it OK to delete it? [y/n]")
-    if user_resp in yes:
-        os.remove(abce_db)
-        print(f"Existing file at {abce_db} deleted.")
-    else:
-        print(f"Please move the file at {abce_db} before running this script again.")
-        print("Terminating...")
-        exit()
-else:
-    print(f"Creating new database file at {abce_db}...")
+    return user_resp
 
+def create_db_file(abce_db):
+    print(f"Creating a new database file at {abce_db}.")
+    db = sqlite3.connect(abce_db)
+    cur = db.cursor()
+    return db, cur
 
-# Create a header-only database seed for projects
-con = sqlite3.connect(abce_db)
-cur = con.cursor()
-cur.execute('''CREATE TABLE xtr_projects
-               (asset_id text, agent_id text, period real, rcec real, rtec real, anpe real)''')
-cur.execute('''CREATE TABLE assets
-               (asset_id text, agent_id text, is_complete text, is_cancelled text, ret_pd real, cap_pmt real)''')
+def create_assets_table(cur):
+    cur.execute("""CREATE TABLE assets
+                 (asset_id text, agent_id text, completion_pd real,
+                 cancellation_pd real, retirement_pd real, cap_pmt real)""")
 
-con.commit()
-con.close()
+def create_WIP_projects_table(cur):
+    cur.execute("""CREATE TABLE WIP_projects
+                 (asset_id text, agent_id text, period real, rcec real,
+                 rtec real, anpe real)""")
 
-print(f"File {abce_db} created.")
+# Set name and path for ABCE database file
+if __name__ == "__main__":
+    # Get the desired filename for the database
+    abce_db = set_database_filename()
+
+    # Check whether the file already exists, and ask user's permission to delete
+    #    if an existing file is found
+    clear_db_file(abce_db)
+
+    # Create a database seed file and associated cursor object
+    db, cur = create_db_file(abce_db)
+
+    # Create the `assets` and `WIP_projects` tables, with associated headers
+    create_assets_table(cur)
+    create_WIP_projects_table(cur)
+
+    # Commit changes and close the connection to the database
+    db.commit()
+    db.close()
+
+    print(f"Database created in file '{abce_db}'.")
