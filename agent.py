552973ffc775e1c6abc1044d9a76819ef4c593a4
biegelk <biegelk3@gmail.com>
2021-03-22 14:31:29 -0500

    add DB data retrieval and WIP_project updating routines from the py_db_updater.py toy script

--- agent.py
+++ agent.py
@@ -53,8 +53,15 @@ class GenCo(Agent):
     def step(self):
         """Controller function to activate all agent behaviors at each time step.
         """
-        self.set_current_step()
-        for id_num in self.portfolio:
+        # Set the current model step
+        self.current_step = self.get_current_step()
+
+        # Get lists of all assets, all WIP projects, and all operating assets
+        self.all_assets = self.get_current_asset_list()
+        self.WIP_projects = self.get_WIP_project_list()
+        self.op_assets = self.get_operating_asset_list()
+
+        for id_num in self.:
             self.portfolio[id_num].step()
         num_units = len(self.portfolio.keys())
         print(f'I\'m GenCo # {self.unique_id}, and I have {num_units} units.')
@@ -63,15 +70,106 @@ class GenCo(Agent):
         # Write the excess unserved demand to a csv
 #        demand_series = pd.DataFrame({'demand': self.available_demand}) * (-1)
 #        demand_file = f"./data/gc{self.unique_id}_demand.csv"
-        subprocess.run(["/bin/bash", "-c", "julia -JabceSysimage.so unit_choice.jl"], start_new_session=True)
+
+        # Run the agent behavior choice algorithm
+        subprocess.run(["/bin/bash", "-c", "julia -JabceSysimage.so agent_choice.jl"], start_new_session=True)
         self.fs.step()
 
-    def set_current_step(self):
+
+    def get_current_step(self):
         """Obtain the current step number from the model.
 
         """
-        self.current_step = self.model.current_step
-        print(f'\n\n Current step = {self.current_step}')
+        current_step = self.model.current_step
+        return current_step
+
+
+    def get_current_asset_list(self):
+        # Get a list of all assets belonging to the current agent where:
+        #  - cancellation_pd is in the future (not currently cancelled)
+        #  - retirement_pd is in the future (not currently retired)
+        db = self.model.db
+        cur = db.cursor()
+        cur.execute(f"SELECT asset_id FROM assets WHERE agent_id = {self.unique_id} AND cancellation_pd > {self.current_step} AND retirement_pd > {self.current_step}")
+        all_asset_list = list(cur.fetchall()[0])
+        return all_asset_list
+
+
+    def get_WIP_project_list(self):
+        # Get a list of all assets belonging to the current agent where:
+        #  - cancellation_pd is in the future (not currently cancelled)
+        #  - completion_pd is in the future (not currently completed)
+        db = self.model.db
+        cur = db.cursor()
+        cur.execute(f"SELECT asset_id FROM assets WHERE agent_id = {self.unique_id} AND completion_pd > {self.current_step} AND cancellation_pd > {self.current_step}")
+        WIP_project_list = list(cur.fetchall()[0])
+        return WIP_project_list
+
+
+    def get_operating_asset_list(self):
+        # Get a list of all assets belonging to the current agent where:
+        #  - cancellation_pd is in the future (not currently cancelled)
+        #  - completion_pd is in the past (already completed)
+        #  - retirement_pd is in the future (not currently retired)
+        db = self.model.db
+        cur = db.cursor()
+        cur.execute(f"SELECT asset_id FROM assets WHERE agent_id = {self.unique_id} AND completion_pd <= {self.current_step} AND cancellation_pd > {self.current_step} AND retirement_pd > {self.current_step}")
+        op_asset_list = list(cur.fetchall()[0])
+        return op_asset_list
+
+
+    def update_assets(self):
+        db = self.model.db
+        cur = db.cursor()
+        if self.WIP_projects:
+            # If there are active construction projects under way:
+            for asset_id in self.WIP_projects:
+                # Select the current project's most recent data record
+                cur.execute(f"SELECT * FROM WIP_projects WHERE asset_id = {asset_id} AND period = {self.current_step - 1}")
+                WIP_project = pd.DataFrame([list(cur.fetchall()[0])], columns = ["asset_id", "agent_id", "period", "rcec", "rtec", "anpe"])
+
+            # TODO: implement stochastic RCEC and RTEC escalation
+            # Currently: no escalation
+
+            if WIP_project.loc[0, "rcec"] - WIP_project.loc[0, "anpe"] <= 0:
+                # This period's authorized expenditures close out the
+                #    remainder of the project. Update the `assets` table to
+                #    reflect completion status.
+                cur.execute(f"UPDATE assets SET completion_pd = {self.current_step} WHERE asset_id = {asset_id}")
+
+            # Update the WIP_project dataframe with new completion data
+            WIP_project.loc[0, "period"] = self.current_step
+            WIP_project.loc[0, "rcec"] = max(WIP_project.loc[0, "rcec"] - WIP_project.loc[0, "anpe"], 0)
+            WIP_project.loc[0, "rtec"] = -= 1
+            WIP_project.loc[0, "anpe"] = 0    # Reset to 0 to avoid inter-period contamination
+
+            # Update the `WIP_projects` database table
+            vals = (WIP_project.loc[0, "asset_id"], WIP_project.loc[0, "agent_id"], 
+                    WIP_project.loc[0, "period"], WIP_project.loc[0, "rcec"],
+                    WIP_project.loc[0, "rtec"], WIP_project.loc[0, "anpe"])
+            cur.execute(f"INSERT INTO WIP_projects VALUES (?, ?, ?, ?, ?, ?)", vals)
+
+        else:
+            # If there are no construction projects in progress:
+            print(f"There are no ongoing construction projects for agent {self.unique_id}."
+
+        # Commit the changes to the database
+        db.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def get_demand_forecast(self):
         # DEPRECATED : will pull from a demand table
