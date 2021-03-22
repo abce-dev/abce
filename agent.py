42b77243eaf1fb5b7c1b391cac63e3424cd796c9
biegelk <biegelk3@gmail.com>
2021-03-22 09:12:18 -0500

    refactoring agent.py and model.py to use database--WIP

--- agent.py
+++ agent.py
@@ -43,7 +43,6 @@ class GenCo(Agent):
         """
         super().__init__(genco_id, model)
         self.assign_parameters('./data/gc_params.yml')
-        self.set_up_portfolio(existing_portfolio)
         self.model = model
         self.fs = fs.AgentFS(model = self.model, agent = self)
         self.tax_rate = self.model.tax_rate
@@ -54,82 +53,11 @@ class GenCo(Agent):
         self.debt_fraction = self.params['debt_fraction']
         self.term_growth_rate = self.params['terminal_growth_rate']
         self.discount_rate = self.params['discount_rate']
-
-
-    def set_up_portfolio(self, existing_portfolio):
-        # DEPRECATED : Replace with "INSERT INTO assets VALUES"
-        """Set up Generator units for each asset in GenCo's assigned portfolio
-
-           Detailed Description
-           --------------------
-           Sets up Generator objects for each unit specified in the GenCo's
-           initial portfolio. The structure of the starting portfolio is still
-           under development.
-
-           For each unit detected in the `existing_portfolio` DataFrame,
-           this function determines its generator type, and sets its unit ID
-           according to the next available ID number as provided by the
-           model's id_register object. A new unit of the appropriate type is
-           initialized, and added to the agent's `self.portfolio` dictionary.
-
-           Parameters
-           ----------
-           existing_portfolio : pandas DataFrame
-               A pandas DataFrame containing at least the following data items:
-               gtype : str
-                   The type of generator. Must correspond to a specified type
-                   in the units.yml file used for the simulation.
-               num_copies : int
-                   The number of identical copies of this unit type to create.
-
-               Optional data items (to be implemented):
-               archetype_id : int
-                   Optional reference to a nonstandard implementation of a 
-                   generation unit. If not specified or set to 0, the default
-                   specifications for the `gtype` generation unit will be
-                   used. Reference to an external table of alternate unit
-                   specs (to be implemented).
-               remaining_life : int
-                   Number of remaining simulation periods before this unit
-                   must be retired.
-
-
-           Returns
-           -------
-           None
-        """
-        self.portfolio = dict()
-        for i in range(len(existing_portfolio.index)):
-            unit_data = pd.Series(existing_portfolio.iloc[i].transpose())
-            for j in range(unit_data['num_copies']):
-                unit_id = self.model.id_register.register_unit(agent_id=self.unique_id)
-                new_unit = gen.Generator(world_model=self.model, agent=self, id_num=unit_id, gtype=unit_data['gtype'], completion=1)
-                self.portfolio[unit_id] = new_unit
+        self.tax_rate = self.params['tax_rate']
 
 
     def step(self):
         """Controller function to activate all agent behaviors at each time step.
-
-           Detailed Description
-           --------------------
-           This function is automatically invoked by the GridModel, once per
-           agent per time step. This function does the following:
-             - Updates the current step number
-             - Updates the status of all WIP and in-service generation units
-                 owned by the agent
-             - Prints an identifying statement to track agent activity
-             - Updates the agent's demand forecast
-             - Totals up the agent's current generation capacity
-             - Evaluates whether additional assets are needed, and cues up new
-               construction projects if so
-
-           Parameters
-           ----------
-           none
-
-           Returns
-           -------
-           None
         """
         self.set_current_step()
         for id_num in self.portfolio:
@@ -137,39 +65,18 @@ class GenCo(Agent):
         num_units = len(self.portfolio.keys())
         print(f'I\'m GenCo # {self.unique_id}, and I have {num_units} units.')
         self.get_demand_forecast()
-        #self.forecast_demand()
         self.evaluate_current_capacity()
-        self.build_new_units(self.assess_supply_adequacy())
+#        self.build_new_units(self.assess_supply_adequacy())
 
         # Write the excess unserved demand to a csv
-        demand_series = pd.DataFrame({'demand': self.available_demand}) * (-1)
-        demand_file = f"./data/gc{self.unique_id}_demand.csv"
-        portfolio_file = f"./data/gc{self.unique_id}_portfolio.yml"
-        demand_series.to_csv(f"./data/gc{self.unique_id}_demand.csv")
-        with open(portfolio_file, "w") as portfile:
-            yaml.dump(self.portfolio, portfile)
+#        demand_series = pd.DataFrame({'demand': self.available_demand}) * (-1)
+#        demand_file = f"./data/gc{self.unique_id}_demand.csv"
         subprocess.run(["/bin/bash", "-c", "julia -JabceSysimage.so unit_choice.jl"], start_new_session=True)
         self.fs.step()
 
     def evaluate_current_capacity(self):
         # DEPRECATED : will be a SELECT capacity FROM assets
         """Sum up all current generation capacity owned by this agent.
-
-        Detailed Description
-        --------------------
-        This function sums up the total capacity of all generation units
-        owned by the utility, including both in-service units and units
-        under construction ('wip' status). This value can then be used to
-        determine whether additional capacity investments are needed.
-
-        Parameters
-        ----------
-        none
-
-        Returns
-        -------
-        None
-
         """
         self.total_capacity = 0
         for unit in self.portfolio.keys():
@@ -180,23 +87,6 @@ class GenCo(Agent):
         # DEPRECATED : will be a DB operation
         # Also has some dummy behavior which needs to be cleared
         """Determine whether future demand exceeds future supply.
-
-        Detailed Description
-        --------------------
-        Compares current total capacity to each demand forecast quantity in
-        the agent's demand visibility window. If a period is observed where
-        demand will be higher than current supply, the agent will start new
-        construction projects in order to compensate.
-
-        Parameters
-        ----------
-        none
-
-        Returns
-        -------
-        num_new_units : int
-            Number of new units to build
-
         """
         supply_surplus = list(self.fs.fsdata['capacity'].iloc[self.current_step+1:self.current_step + len(self.demand_forecast)+1] - self.demand_forecast)
         self.available_demand = supply_surplus
@@ -209,20 +99,6 @@ class GenCo(Agent):
     def build_new_units(self, new_units):
         # DEPRECATED : will be handled by Julia
         """Start a new construction project, and add to agent's portfolio.
-
-        Detailed Description
-        --------------------
-        Current behavior: builds only unit_1 type units.
-
-        Parameters
-        ----------
-        num_new_units : int
-            Number of new units to build.
-
-        Returns
-        -------
-        None
-
         """
         for i in range(new_units):
             unit_id = self.model.id_register.register_unit(agent_id = self.unique_id)
@@ -239,53 +115,8 @@ class GenCo(Agent):
     def get_demand_forecast(self):
         # DEPRECATED : will pull from a demand table
         """ Obtain the current demand visibility window from the model.
-
-            Detailed Description
-            --------------------
-            The model provides a fixed look-ahead window of the deterministic
-            demand data provided in an input file. The agent retrieves this
-            information and uses it for planning.
-
         """
         if self.model.demand_NTF != []:
             self.demand_forecast = self.model.demand_NTF
             self.current_demand = self.demand_forecast[0]
-
-    def forecast_demand(self):
-        # DEPRECATED : may be completely removable
-        """Determine lead time until next unmanageable demand increase.
-
-           Detailed Description
-           --------------------
-           Loops over the current demand visibility window, and determines
-           whether (and when) a capacity shortfall will occur.
-
-           The time step at which this will happen is saved in the variable
-           `self.next_demand_increase_period`.
-
-           This function is not currently used, but its capability will be
-           needed to allow agents to decide which units can be built in time
-           to meet the additional demand.
-
-           Parameters
-           ----------
-           none
-
-           Returns
-           -------
-           None
-
-        """
-        # Currently can only detect a single upcoming step-change in demand
-        for i in range(len(self.demand_forecast)):
-            if self.demand_forecast[i] == self.current_demand:
-                continue
-            if self.demand_forecast[i] > self.current_demand:
-                print(f'Demand will increase in {i} periods, from {self.current_demand} to {self.demand_forecast[i]}.')
-                self.next_demand_increase_period = self.current_step + i
-                return
-            elif self.demand_forecast[i] < self.current_demand:
-                print(f'Demand will decrease in {i} periods, from {self.current_demand} to {self.demand_forecast[i]}.')
-                self.next_demand_increase_period = self.current_step + i
-                return
 
--- model.py
+++ model.py
@@ -16,41 +16,43 @@ class GridModel(Model):
         self.num_agents = n
         self.current_step = -1
         self.future_vis = 4  # Number of time steps agents can see into the future
-        self.tax_rate = 0.265 # Corporate tax rate
 
         # Initialize database for managing asset and WIP construction project data
+        self.db_file = db_file
         sp = subprocess.run(["python3 seed_creator.py"], shell=True)
-        self.db, self.cur = load_database(db_file)
+        self.db, self.cur = load_database(self.db_file)
 
         # Load default unit data and demand profile
-        self.load_unit_data(unit_data_file)
+        self.unit_types, self.unit_data = self.load_unit_data(unit_data_file)
         self.get_true_market_data(demand_data_file)
         self.set_demand_visibility_window()
 
         # Set up a public register of unit IDs
         # DEPRECATED : will be replaced by a "SELECT asset_id FROM assets"
-        self.id_register = id_register.ID_register()
-
-        # Set up a public demand-history data series
-        # DEPRECATED : will be implemented via DB
-        #self.demand_history = demand_history.DemandHistory(self.prev_demand[0])
+#        self.id_register = id_register.ID_register()
 
         # Define the agent schedule, using randomly-ordered agent activation
         self.schedule = RandomActivation(self)
-
-        # Load the agents' starting portfolios
-        c_portfolio = pd.read_csv('./data/portfolios.csv', index_col='agent', skipinitialspace=True)
 
         # Create agents
         for i in range(self.num_agents):
             gc = GenCo(i, self, c_portfolio.loc[i+1])
             self.schedule.add(gc)
 
+        # Load the agents' starting portfolios
+        initial_assets = pd.read_csv('./data/portfolios.csv', skipinitialspace=True)
+
+        # Add all initial assets to the database
+        add_initial_assets_to_db(initial_assets, self.cur, self.db)
+
+
     def load_unit_data(self, filename):
         unit_file = open(filename)
-        self.unit_data = yaml.load(unit_file, Loader=yaml.FullLoader)
-        self.unit_data = pd.DataFrame.from_dict(self.unit_data, orient='index')
-        self.unit_types = self.unit_data.index
+        unit_data = yaml.load(unit_file, Loader=yaml.FullLoader)
+        unit_data = pd.DataFrame.from_dict(unit_data, orient='index')
+        unit_types = self.unit_data.index
+        return unit_types, unit_data
+
 
     def get_true_market_data(self, filename):
         market_file = open(filename)
@@ -59,14 +61,29 @@ class GridModel(Model):
         self.true_demand_profile = market_data['demand']
         self.eprices = market_data['prices']
 
+
     def set_demand_visibility_window(self):
         self.demand_NTF = self.true_demand_profile[self.current_step:self.current_step + self.future_vis]
 
+
+    def add_initial_assets_to_db(initial_assets, cur, db):
+        for i in range(len(initial_assets)):
+            for j in range(initial_assets.loc[i, "num_copies"]):
+                asset_id = get_next_asset_id(self.db, self.cur)
+                agent_id = initial_assets.loc[i, "agent_id"]
+                completion_pd = 0
+                cancellation_pd = 9999
+                retirement_pd = initial_assets.loc[i, "useful_life"]
+                capital_payment = self.unit_data.loc[i, "overnight_cost"] / initial_assets.loc[i, "useful_life"]
+                vals = (asset_id, agent_id, completion_pd, cancellation_pd, retirement_pd, capital_payment)
+                cur.execute("""INSERT INTO assets VALUES
+                               ({asset_id}, {agent_id}, {completion_pd},
+                                {cancellation_pd}, {retirement_pd},
+                                {capital_payment})""")
+
+
     def step(self):
         ''' Advance the model by one step. '''
-        # Move next future demand data point to the historical register
-        #if self.current_step != -1:
-        #    self.demand_history.add_data(self.demand_NTF[0])
         self.current_step += 1
         self.set_demand_visibility_window()
         self.schedule.step()
