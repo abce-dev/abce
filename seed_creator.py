86da300c35368a7257d0d28489e5dd4b324949f9
kbiegel <kbiegel@anl.gov>
2021-04-21 15:37:56 -0500

    updating to use create_all_tables()

--- seed_creator.py
+++ seed_creator.py
@@ -57,72 +57,9 @@ abce_tables = {"WIP_projects":
                  [("lamda", "real")]
               }
 
-# table 4: unit_specs
-#
-#    - unit_type    : text : seed_creator
-#    - fuel_type    : text : seed_creator
-#    - capacity     : real : seed_creator
-#    - uc_x         : real : seed_creator
-#    - d_x          : real : seed_creator
-#    - heat_rate    : real : seed_creator
-#    - VOM          : real : seed_creator
-#    - FOM          : real : seed_creator
-#    - unit_life    : real : seed_creator
-#    - CF           : real : seed_creator
-#    - fuel_cost    : real : seed_creator
-# table 5: demand
-#
-#    - period       : real : model
-#    - demand       : real : model
-# table 6: price duration curve data
-#
-#    - lamda        : real : model
-
-
-
-
-
-
-# table 1: WIP_projects
-#       HEADER         TYPE   INIT BY  STEPPED BY
-#    - asset_id      : text : Julia,   Python
-#    - agent_id      : text : Julia,   Python
-#    - period        : real : Julia,   Python
-#    - rcec          : real : Julia,   Python
-#    - rtec          : real : Julia,   Python
-#    - anpe          : real : Julia,   Julia 
-#
-# table 2: assets
-#       HEADER            TYPE   INIT BY        STEPPED BY
-#    - asset_id         : text : Python/Julia,  n/a
-#    - agent_id         : text : Python/Julia,  n/a
-#    - revealed         : text :
-#    - completion_pd    : text : Python/Julia,  Python
-#    - cancellation_pd  : text : Julia,         Julia
-#    - retirement_pd    : real : Python/Julia,  Python
-#    - total_capex      : real : Python,        Python
-#    - cap_pmt          : real : Python/Julia,  Python
-#    - unit_type        : text : Python/Julia,  n/a
-#
-# table 3: agent_params
-#       HEADER             TYPE   SET BY
-#    - agent_id          : text : model
-#    - discount_rate     : real : model
-#    - tax_rate          : real : model
-#    - term_growth_rate  : real : model
-#    - debt_fraction     : real : model
-#    - debt_cost         : real : model
-#    - equity_cost       : real : model
-#    - interest_cap      : real : model
-#
 
 import sqlite3
 import os
-
-def set_database_filename():
-    abce_db = "./abce_db.db"
-    return abce_db
-
 
 def clear_db_file(abce_db):
     if os.path.exists(abce_db):
@@ -160,40 +97,6 @@ def make_table(cur, table_name):
     cur.execute(cmd)
 
 
-def create_assets_table(cur):
-    cur.execute("""CREATE TABLE assets
-                 (asset_id text, agent_id text, revealed text, unit_type text,
-                  completion_pd real, cancellation_pd real, retirement_pd real,
-                  total_capex real, cap_pmt real)""")
-
-
-def create_WIP_projects_table(cur):
-    cur.execute("""CREATE TABLE WIP_projects
-                 (asset_id text, agent_id text, period real, rcec real,
-                  rtec real, anpe real)""")
-
-
-def create_agent_params_table(cur):
-    cur.execute("""CREATE TABLE agent_params
-                   (agent_id text, discount_rate real, tax_rate real,
-                    term_growth_rate real, debt_fraction real, debt_cost real,
-                    equity_cost real, interest_cap real)""")
-
-def create_unit_specs_table(cur):
-    cur.execute("""CREATE TABLE	unit_specs
-                   (unit_type text, fuel_type text, capacity real, uc_x real,
-                    d_x real, heat_rate real, VOM real, FOM real,
-                    unit_life real, CF real, fuel_cost real)""")
-
-def create_demand_table(cur):
-    cur.execute("CREATE TABLE demand (period real, demand real)")
-
-
-def create_price_curve_table(cur):
-    cur.execute("CREATE TABLE price_curve (lamda real)")
-
-
-
 def create_all_tables(cur):
     for table in abce_tables:
         make_table(cur, table)
@@ -202,7 +105,7 @@ def create_all_tables(cur):
 # Set name and path for ABCE database file
 if __name__ == "__main__":
     # Get the desired filename for the database
-    abce_db = set_database_filename()
+    abce_db = "./abce_db.db"
 
     # Check whether the file already exists, and ask user's permission to delete
     #    if an existing file is found
@@ -213,12 +116,7 @@ if __name__ == "__main__":
 
     # Create the `assets`, `WIP_projects`, and `agent_params` tables, 
     #    including headers
-    create_assets_table(cur)
-    create_WIP_projects_table(cur)
-    create_agent_params_table(cur)
-    create_unit_specs_table(cur)
-    create_demand_table(cur)
-    create_price_curve_table(cur)
+    create_all_tables(cur)
 
     # Commit changes and close the connection to the database
     db.commit()
