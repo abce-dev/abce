7747312c0ee974d54e8e66fac1fb5b703d3c302a
kbiegel <kbiegel@anl.gov>
2021-04-27 11:34:48 -0500

    final DB is saved to an xlsx file for easy user viewing

--- run.py
+++ run.py
@@ -3,15 +3,24 @@ from mesa.time import RandomActivation
 from agent import GenCo
 from model import GridModel
 import yaml
+import pandas as pd
 
 settings_file = "./settings.yml"
 with open(settings_file) as setfile:
     settings = yaml.load(setfile, Loader=yaml.FullLoader)
 
 num_steps = settings["num_steps"]
+output_file = settings["results_file"]
 
 # Run the model
 if __name__ == '__main__':
     abce_model = GridModel(settings_file)
     for i in range(num_steps):
         abce_model.step()
+
+db_tables = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", abce_model.db)
+with pd.ExcelWriter(output_file) as writer:
+    for i in range(len(db_tables)):
+        table = db_tables.loc[i, "name"]
+        df = pd.read_sql_query(f"SELECT * FROM {table}", abce_model.db)
+        df.to_excel(writer, sheet_name=f"{table}", engine="openpyxl")
