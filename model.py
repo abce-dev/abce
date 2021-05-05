8dab0a86a40fd074d9d685bb72c14ca6482f0c10
kbiegel <kbiegel@anl.gov>
2021-05-05 16:02:14 -0500

    model.py code cleanup/moving code into functions, employing member data correctly

--- model.py
+++ model.py
@@ -26,25 +26,23 @@ class GridModel(Model):
         self.first_agent_id = settings["first_agent_id"]
         self.first_asset_id = settings["first_asset_id"]
         self.total_forecast_horizon = settings["total_forecast_horizon"]
-        try:
+
+        # Determine setting for use of a precomputed price curve
+        self.use_precomputed_price_curve = True
+        if "use_precomputed_price_curve" in settings:
             self.use_precomputed_price_curve = settings["use_precomputed_price_curve"]
-        except:
-            # Default to `True` but warn the user
-            print("No setting specified for price curve generation; will use precomputed curve.")
-            self.use_precomputed_price_curve = True
 
-
+        # Initialize the model one time step before the true start date
         self.current_step = -1
 
         # Initialize database for managing asset and WIP construction project data
         self.db_file = db_file
         self.db, self.cur = sc.create_database(self.db_file, args.replace)
 
-
         # Load unit type specifications and fuel costs
         self.unit_types, self.unit_specs = self.load_unit_specs(unit_specs_file)
         self.fuel_costs = pd.read_csv(fuel_data_file)
-        self.add_units_to_db(self.db, self.cur, self.unit_specs, self.fuel_costs)
+        self.add_units_to_db()
 
         # Load all-period demand data into the database
         self.load_demand_data_to_db(settings)
@@ -59,25 +57,12 @@ class GridModel(Model):
 
         # Check whether a market price subsidy is in effect, and its value
         self.set_market_subsidy(settings)
-        # Set up the price curve according to specifications in settings
-        if self.use_precomputed_price_curve:
-            price_duration_data = pc.load_time_series_data(price_curve_data_file, file_type="price", subsidy=self.subsidy_amount)
-        else:
-            # Create the systemwide merit order curve
-            merit_curve = pc.create_merit_curve(self.db, self.current_step)
-            pc.plot_curve(merit_curve, plot_name="merit_curve.png")
-            # Load five-minute demand data from file
-            demand_data = pc.load_time_series_data(time_series_data_file, file_type="load", peak_demand=settings["peak_demand"])
-            pc.plot_curve(demand_data, plot_name="demand_curve.png")
-            # Create the final price duration curve
-            price_duration_data = pc.compute_price_duration_curve(demand_data, merit_curve, settings["price_cap"])
-            # Save a plot of the price duration curve
-            pc.plot_curve(price_duration_data, plot_name="price_duration.png")
-
+        # Create an appropriate price duration curve
+        self.create_price_duration_curve(settings)
 
         # Save price duration data to the database
-        for i in range(len(price_duration_data)):
-            self.cur.execute(f"INSERT INTO price_curve VALUES ({price_duration_data[i]})")
+        for i in range(len(self.price_duration_data)):
+            self.cur.execute(f"INSERT INTO price_curve VALUES ({self.price_duration_data[i]})")
         self.db.commit()
 
 
@@ -87,28 +72,28 @@ class GridModel(Model):
         return unit_types, unit_specs
 
 
-    def add_units_to_db(self, db, cur, unit_specs, fuel_costs):
-        for i in range(len(unit_specs)):
+    def add_units_to_db(self):
+        for i in range(len(self.unit_specs)):
             # Get unit data from file
-            unit_type = unit_specs.loc[i, "unit_type"]
-            fuel_type = unit_specs.loc[i, "fuel_type"]
-            capacity = unit_specs.loc[i, "capacity"]
-            uc_x = unit_specs.loc[i, "uc_x"]
-            d_x = unit_specs.loc[i, "d_x"]
-            heat_rate = unit_specs.loc[i, "heat_rate"]
-            VOM = unit_specs.loc[i, "VOM"]
-            FOM = unit_specs.loc[i, "FOM"]
-            unit_life = unit_specs.loc[i, "unit_life"]
-            CF = unit_specs.loc[i, "CF"]
+            unit_type = self.unit_specs.loc[i, "unit_type"]
+            fuel_type = self.unit_specs.loc[i, "fuel_type"]
+            capacity = self.unit_specs.loc[i, "capacity"]
+            uc_x = self.unit_specs.loc[i, "uc_x"]
+            d_x = self.unit_specs.loc[i, "d_x"]
+            heat_rate = self.unit_specs.loc[i, "heat_rate"]
+            VOM = self.unit_specs.loc[i, "VOM"]
+            FOM = self.unit_specs.loc[i, "FOM"]
+            unit_life = self.unit_specs.loc[i, "unit_life"]
+            CF = self.unit_specs.loc[i, "CF"]
             # Incorporate unit fuel cost data from the fuel costs file
-            fuel_cost = fuel_costs[fuel_costs.fuel_type == fuel_type].reset_index().loc[0, "cost_per_mmbtu"]
+            fuel_cost = self.fuel_costs[self.fuel_costs.fuel_type == fuel_type].reset_index().loc[0, "cost_per_mmbtu"]
 
             # Insert the values into the unit_specs DB table
-            cur.execute(f"""INSERT INTO unit_specs VALUES
+            self.cur.execute(f"""INSERT INTO unit_specs VALUES
                             ('{unit_type}', '{fuel_type}', {capacity}, {uc_x},
                              {d_x}, {heat_rate}, {VOM}, {FOM}, {unit_life},
                              {CF}, {fuel_cost})""")
-            db.commit()
+            #db.commit()
 
 
     def load_demand_data_to_db(self, settings):
@@ -129,6 +114,22 @@ class GridModel(Model):
             self.subsidy_enabled = settings["enable_subsidy"]
             if self.subsidy_enabled and "subsidy_amount" in settings:
                 self.subsidy_amount = settings["subsidy_amount"]
+
+    def create_price_duration_curve(self, settings):
+        # Set up the price curve according to specifications in settings
+        if self.use_precomputed_price_curve:
+            self.price_duration_data = pc.load_time_series_data(settings["price_curve_data_file"], file_type="price", subsidy=self.subsidy_amount)
+        else:
+            # Create the systemwide merit order curve
+            self.merit_curve = pc.create_merit_curve(self.db, self.current_step)
+            pc.plot_curve(self.merit_curve, plot_name="merit_curve.png")
+            # Load five-minute demand data from file
+            self.demand_data = pc.load_time_series_data(settings["time_series_data_file"], file_type="load", peak_demand=settings["peak_demand"])
+            pc.plot_curve(self.demand_data, plot_name="demand_curve.png")
+            # Create the final price duration curve
+            self.price_duration_data = pc.compute_price_duration_curve(self.demand_data, self.merit_curve, settings["price_cap"])
+            # Save a plot of the price duration curve
+            pc.plot_curve(self.price_duration_data, plot_name="price_duration.png")
 
 
     def step(self):
