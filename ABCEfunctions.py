b6f0ff25f2eadaf54a7d3b3d49fd1198243a8927
kbiegel <kbiegel@anl.gov>
2021-05-21 16:16:49 -0500

    updated DB call to use SQL MAX(...) function

--- ABCEfunctions.py
+++ ABCEfunctions.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 
 def get_next_asset_id(db, first_asset_id):
-    next_id = first_asset_id
+    max_id = first_asset_id
 
     asset_list = pd.read_sql("SELECT asset_id FROM assets", db)
     if len(asset_list) != 0:
-        next_id = max(asset_list["asset_id"]) + 1
+        max_id = pd.read_sql("SELECT MAX(asset_id) FROM assets", db).iloc[0, 0] + 1
 
-    return next_id
+    return max_id
 
 
 
