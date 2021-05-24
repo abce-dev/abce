e52b4cf1360b873cfbc76930c9f27f7d0afaa622
kbiegel <kbiegel@anl.gov>
2021-05-24 10:03:52 -0500

    refactoring get_next_asset_id to use SQL 'SELECT MAX(...)' function more efficiently

--- ABCEfunctions.py
+++ ABCEfunctions.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 
 def get_next_asset_id(db, first_asset_id):
-    max_id = first_asset_id
-
-    asset_list = pd.read_sql("SELECT asset_id FROM assets", db)
-    if len(asset_list) != 0:
-        max_id = pd.read_sql("SELECT MAX(asset_id) FROM assets", db).iloc[0, 0] + 1
+    max_id = pd.read_sql("SELECT MAX(asset_id) FROM assets", db).iloc[0, 0]
+    if max_id == None:
+        next_id = first_asset_id
+    else:
+        next_id = max_id + 1
 
     return max_id
 
