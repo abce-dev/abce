4ceaecb3f490d0f2aa0fa6ef9c1059f3f04f0045
kbiegel <kbiegel@anl.gov>
2021-06-10 11:06:03 -0500

    script to create a Julia project environment (Manifest.toml and Project.toml) including all packages needed for both ALEAF and ABCE

---
+++ make_julia_environment.jl
@@ -0,0 +1,65 @@
+using Pkg
+
+# Activate local environment
+Pkg.activate(".")
+
+julia_pkg_list = ["CSV",
+                  "Cbc",
+                  "Conda",
+                  "DataFrames",
+                  "FileIO",
+                  "GLPK",
+                  "HDF5",
+                  "Infiltrator",
+                  "Ipopt",
+                  "JLD2",
+                  "JSON",
+                  "JuMP",
+                  "LinearAlgebra",
+                  "MathOptInterface",
+                  "Memento",
+                  "PowerModels",
+                  "PyCall",
+                  "SQLite",
+                  "XLSX"]
+
+conda_list =     ["numpy",
+                  "scipy",
+                  "matplotlib",
+                  "pandas"]
+
+
+
+
+# Add Julia libraries needed for ALEAF
+# Add and build the optimizer packages
+# CPLEX version 0.6.0 is required for compatibility with CPLEX 12.8
+println("Adding CPLEX")
+Pkg.add(Pkg.PackageSpec(;name="CPLEX", version="0.6.0"))
+println("Building CPLEX...")
+Pkg.build("CPLEX")
+println("CPLEX built.")
+
+println("Adding Gurobi")
+Pkg.add("Gurobi")
+println("Building Gurobi...")
+Pkg.build("Gurobi")
+println("Gurobi built.")
+
+# Add the non-optimizer packages (which don't need to be built)
+for i=1:size(julia_pkg_list)[1]
+    println(string("Adding ", julia_pkg_list[i]))
+    Pkg.add(julia_pkg_list[i])
+end
+
+println("All libraries added to the environment.")
+
+# Ensure all non-default Python packages are installed via Conda.jl
+println("Ensuring Conda packages are installed...")
+using Conda
+for i=1:size(conda_list)[1]
+    println(string("Adding ", conda_list[i]))
+    Conda.add(conda_list[i])
+end
+
+println("All packages and Python libraries loaded successfully.")
