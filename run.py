7aa0e8f73fa4050ce68a1610d8d2d32989ffa04e
kbiegel <kbiegel@anl.gov>
2021-11-16 18:23:55 -0600

    removing reference to outdated member data variable name

--- run.py
+++ run.py
@@ -122,7 +122,7 @@ def run_model():
             final_db.to_excel(writer, sheet_name=f"{table}", engine="openpyxl")
 
     # Postprocess A-LEAF results
-    ABCEfunctions.process_outputs(settings, abce_model.ABCE_output_path)
+    ABCEfunctions.process_outputs(settings, abce_model.ABCE_output_data_path)
 
 
 # Run the model
