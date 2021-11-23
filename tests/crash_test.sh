523327e61d12e7a9d12f5b5601270689cbc5581e
kbiegel <kbiegel@anl.gov>
2021-11-23 16:28:43 -0600

    changing all instances of 'echo' to 'printf' to allow display of helpful newlines

--- tests/crash_test.sh
+++ tests/crash_test.sh
@@ -5,17 +5,17 @@
 #   configuration and settings.
 
 # Describe the test to the user
-echo "TEST: ABCE simple crash test"
-echo "This test will run ABCE using whatever settings currently exist in settings.yml."
-echo "If ABCE does not crash (i.e. it runs to completion and returns an exit code of 0, the test passes."
-echo "If something happens and ABCE returns any non-zero return code, the test fails.\n"
-echo "Warning: This test does NOT guarantee code is working correctly! It only detects fatal crashes."
+printf "TEST: ABCE simple crash test\n\n"
+printf "This test will run ABCE using whatever settings currently exist in settings.yml.\n"
+printf "If ABCE does not crash (i.e. it runs to completion and returns an exit code of 0, the test passes.\n"
+printf "If something happens and ABCE returns any non-zero return code, the test fails.\n\n"
+printf "Warning: This test does NOT guarantee code is working correctly! It only detects fatal crashes.\n\n"
 
 # Use the CL argument to set the path to run.py
 # If no CL was given, throw an error and exit with return code 1
 if [ -z "${1+x}" ]
 then
-    echo "No path specified for the crash test; aborting."
+    printf "No path specified for the crash test; aborting.\n\n"
     exit 1
 else
     run_script_path="$1"
