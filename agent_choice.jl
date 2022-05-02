a6ffd509038738edb9ab940713ea655e4c0502ac
kbiegel <kbiegel@anl.gov>
2022-05-02 05:08:59 -0500

    adding dispatch routine into agent_choice.jl, not hooked up yet

--- agent_choice.jl
+++ agent_choice.jl
@@ -47,7 +47,9 @@ settings = YAML.load_file(settings_file)
 # Include local ABCE functions module
 julia_ABCE_module = joinpath(settings["ABCE_abs_path"], "ABCEfunctions.jl")
 include(julia_ABCE_module)
-using .ABCEfunctions
+dispatch_module = joinpath(settings["ABCE_abs_path"], "dispatch.jl")
+include(dispatch_module)
+using .ABCEfunctions, .Dispatch
 
 @info "Packages loaded successfully."
 
@@ -83,43 +85,92 @@ agent_params = get_agent_params(db, agent_id)
 d = agent_params[1, :discount_rate]
 
 # System parameters
-# Read unit operational data (unit_data) and number of unit types (num_types)
-unit_data, num_types = get_unit_specs(db)
-@info unit_data
+# Read unit operational data (unit_specs) and number of unit types (num_types)
+unit_specs, num_types = get_unit_specs(db)
+@info unit_specs
 num_alternatives = num_types * (num_lags + 1)
 
 # Ensure that forecast horizon is long enough to accommodate the end of life
 #   for the most long-lived possible unit
-fc_pd = set_forecast_period(unit_data, num_lags)
+fc_pd = set_forecast_period(unit_specs, num_lags)
 
 # Load the demand data
-available_demand = get_demand_forecast(db, pd, agent_id, fc_pd, settings)
+total_demand = get_demand_forecast(db, pd, agent_id, fc_pd, settings)
 
 # Extend the unserved demand data to match the total forecast period (constant projection)
-available_demand = get_net_demand(db, pd, agent_id, fc_pd, available_demand)
+available_demand = get_net_demand(db, pd, agent_id, fc_pd, total_demand)
 @info "Available demand:"
 @info DataFrame(net_demand = available_demand)[1:10, :]
 
 # Load the price data
 price_curve = DBInterface.execute(db, "SELECT * FROM price_curve") |> DataFrame
 
-# Add empty column for project NPVs in unit_data
-unit_data[!, :FCF_NPV] = zeros(Float64, num_types)
+# Add empty column for project NPVs in unit_specs
+unit_specs[!, :FCF_NPV] = zeros(Float64, num_types)
 
-PA_uids, PA_fs_dict = set_up_project_alternatives(unit_data, asset_counts, num_lags, fc_pd, agent_params, price_curve, db, pd)
+PA_uids, PA_fs_dict = set_up_project_alternatives(unit_specs, asset_counts, num_lags, fc_pd, agent_params, price_curve, db, pd)
 
 @info "Project alternatives:"
 @info PA_uids
 
 if pd == 0
     @info "Unit data loaded:"
-    @info unit_data
+    @info unit_specs
 end
 
 @info "Data initialized."
 
+
+# Set up portfolio projections
+@info "Setting up dispatch portfolios..."
+num_years = 10
+system_portfolios = Dict()
+for y = 0:num_years
+    # Retrieve a list of all units expected to be operational during this year,
+    #   grouped by unit type
+    year_portfolio = DBInterface.execute(db, "SELECT unit_type, COUNT(unit_type) FROM assets WHERE completion_pd <= $y AND retirement_pd > $y AND cancellation_pd > $y GROUP BY unit_type") |> DataFrame
+
+    # Rename the COUNT(unit_type) column to num_units
+    rename!(year_portfolio, Symbol("COUNT(unit_type)") => :num_units)
+
+    # Add the year-i portfolio to the dictionary
+    system_portfolios[y] = year_portfolio
+end
+
+@info "Dispatch portfolios set up."
+
+# Run dispatch for each forecast year
+@info "Running the dispatch simulation for $num_years years..."
+num_repdays = 20
+ts_data = Dispatch.load_ts_data(
+              joinpath(settings["ABCE_abs_path"],
+                       "inputs",
+                       "ALEAF_inputs"
+              ),
+              num_repdays
+          )
+
+# Set up dataframes to record all results
+all_prices, all_gc_results = Dispatch.set_up_results_dfs()
+
+for y = 0:num_years
+    @info "Start of year $y dispatch simulation..."
+    # Retrieve the current year's expected portfolio
+    year_portfolio = system_portfolios[y]
+
+    # Set up and run the dispatch simulation for this year
+    Dispatch.run_annual_dispatch(y, year_portfolio, total_demand[y+1, :demand], ts_data, unit_specs, all_gc_results, all_prices)
+    @info "Year $y dispatch complete."
+end
+
+Dispatch.save_raw_results(all_prices, all_gc_results)
+
+@info "Postprocessing dispatch simulation..."
+final_profit_pivot, all_gc_results, all_prices = Dispatch.postprocess_results(system_portfolios, all_prices, all_gc_results, ts_data, unit_specs)
+
+
 ###### Set up the model
-m = set_up_model(settings, PA_uids, PA_fs_dict, available_demand, asset_counts, agent_params, unit_data, pd)
+m = set_up_model(settings, PA_uids, PA_fs_dict, available_demand, asset_counts, agent_params, unit_specs, pd)
 
 ###### Solve the model
 @info "Solving optimization problem..."
@@ -138,13 +189,13 @@ all_results = hcat(PA_uids, DataFrame(units_to_execute = unit_qty))
 
 
 ###### Save the new units into the `assets` and `WIP_projects` DB tables
-postprocess_agent_decisions(all_results, unit_data, db, pd, agent_id)
+postprocess_agent_decisions(all_results, unit_specs, db, pd, agent_id)
 
 ##### Authorize ANPE for all current WIP projects
 # Retrieve all WIP projects
 WIP_projects = get_WIP_projects_list(db, pd, agent_id)
 # Authorize ANPE for the upcoming period (default: $1B/year)
-authorize_anpe(db, agent_id, pd, WIP_projects, unit_data)
+authorize_anpe(db, agent_id, pd, WIP_projects, unit_specs)
 
 # End
 @info "Julia: finishing"
