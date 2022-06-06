a6a74883814df28e6662e7494bfa0a58f2783060
kbiegel <kbiegel@anl.gov>
2022-06-06 10:30:33 -0500

    changing base_period to reflect the period during which data was generated (current_pd - 1)

--- price_curve.py
+++ price_curve.py
@@ -74,7 +74,7 @@ def organize_price_data(file_name, price_df, current_pd, output_type):
     lamda["lamda"] = lamda["lamda"].apply(lambda x: min(9001, x))
 
     # Set the base_pd to the current period
-    lamda["base_pd"] = current_pd
+    lamda["base_pd"] = current_pd - 1
 
     # Format as np.array
     if output_type == "np.array":
