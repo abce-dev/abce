1f41b25f4a66b56ae1d6c558d1d8175552f00c47
kbiegel <kbiegel@anl.gov>
2022-06-14 09:22:45 -0500

    removing old CPLEX dependency

--- make_julia_environment.jl
+++ make_julia_environment.jl
@@ -35,7 +35,8 @@ conda_list = ["numpy",
 # Add and build the optimizer packages
 # CPLEX version 0.6.0 is required for compatibility with CPLEX 12.8
 println("Adding CPLEX")
-Pkg.add(Pkg.PackageSpec(;name="CPLEX", version="0.6.0"))
+Pkg.add("CPLEX")
+#Pkg.add(Pkg.PackageSpec(;name="CPLEX", version="0.6.0"))
 println("Building CPLEX...")
 Pkg.build("CPLEX")
 println("CPLEX built.")
