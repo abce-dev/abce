0b6f3d472c2e7e94bd565645b976367ce374be85
kbiegel <kbiegel@anl.gov>
2022-08-02 18:17:08 +0200

    adding -c make clean style optional CL argument to make_julia_environment, and changing that script's printlns into info

--- make_julia_environment.jl
+++ make_julia_environment.jl
@@ -1,7 +1,4 @@
-using Pkg
-
-# Activate local environment
-Pkg.activate(".")
+using Pkg, Logging, ArgParse
 
 julia_pkg_list = ["ArgParse",
                   "CSV",
@@ -32,31 +29,61 @@ conda_list = ["numpy",
               "matplotlib",
               "pandas"]
 
+# Set up command-line parser
+s = ArgParseSettings()
+@add_arg_table s begin
+    "-c"
+        help = "make clean, i.e. delete existing *.toml files in current directory"
+        action = :store_true
+end
+CLI_args = parse_args(s)
+
+# If the user specifies the -c flag, try to delete Manifest.toml and
+#   Project.toml, if they exist in the current directory
+if CLI_args["c"] == true
+    files_to_delete = ["Manifest.toml", "Project.toml"]
+    for dfile in files_to_delete
+        try
+            rm(abspath(dfile))
+            @info "Removed file $dfile"
+        catch e
+            if occursin("no such file", e.msg)
+                current_dir = @__DIR__
+                @info "No extant file $dfile in the current directory, $current_dir."
+            else
+                throw(e)
+            end
+        end
+    end
+end
+
+# Activate local environment
+Pkg.activate(".")
+
 # Add Julia libraries needed for ALEAF
 # Add and build the optimizer packages
-# CPLEX version 0.6.0 is required for compatibility with CPLEX 12.8
-println("Adding CPLEX")
+@info "Adding CPLEX"
 Pkg.add("CPLEX")
-#Pkg.add(Pkg.PackageSpec(;name="CPLEX", version="0.6.0"))
-println("Building CPLEX...")
+@info "Building CPLEX..."
 Pkg.build("CPLEX")
-println("CPLEX built.")
+@info "CPLEX built."
 
 # Add the non-optimizer packages (which don't need to be built)
 for i=1:size(julia_pkg_list)[1]
-    println(string("Adding ", julia_pkg_list[i]))
+    @info string("Adding ", julia_pkg_list[i])
     Pkg.add(julia_pkg_list[i])
+    Pkg.build(julia_pkg_list[i])
 end
 
-println("All libraries added to the environment.")
+@info "All libraries added to the environment."
 
 # Ensure all non-default Python packages are installed via Conda.jl
-println("Ensuring Conda packages are installed...")
+@info "Ensuring Conda packages are installed..."
 Pkg.build("Conda")
 using Conda
 for i=1:size(conda_list)[1]
-    println(string("Adding ", conda_list[i]))
+    @info string("Adding ", conda_list[i])
     Conda.add(conda_list[i])
 end
 
-println("All packages and Python libraries loaded successfully.")
+@info "All packages and Python libraries loaded successfully."
