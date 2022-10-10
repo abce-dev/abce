d794735eea6fdfa22f6ede37784294aac2d66b0f
kbiegel <kbiegel@anl.gov>
2022-10-10 11:40:18 -0500

    deletes preexisting toml files by default; includes CPLEX in the default issue-handling method

--- make_julia_environment.jl
+++ make_julia_environment.jl
@@ -1,58 +1,50 @@
 using Pkg, Logging, ArgParse
 
-julia_pkg_list = ["ArgParse",
-                  "CSV",
-                  "Conda",
-                  "DataFrames",
-                  "FileIO",
-                  "GLPK",
-                  "HDF5",
-                  "Infiltrator",
-                  "Ipopt",
-                  "JLD2",
-                  "JSON",
-                  "JuMP",
-                  "LinearAlgebra",
-                  "MathOptInterface",
-                  "Memento",
-                  "PackageCompiler",
-                  "PowerModels",
-                  "PyCall",
-                  "SCIP",
-                  "SQLite",
-                  "Tables",
-                  "XLSX",
-                  "YAML",
-                  "Cbc",
-                  "SCIP",
-                  "HiGHS"]
+julia_pkg_list = [
+    "ArgParse",
+    "CPLEX",
+    "CSV",
+    "Conda",
+    "DataFrames",
+    "FileIO",
+    "GLPK",
+    "HDF5",
+    "Infiltrator",
+    "Ipopt",
+    "JLD2",
+    "JSON",
+    "JuMP",
+    "LinearAlgebra",
+    "MathOptInterface",
+    "Memento",
+    "PackageCompiler",
+    "PowerModels",
+    "PyCall",
+    "SCIP",
+    "SQLite",
+    "Tables",
+    "XLSX",
+    "YAML",
+    "Cbc",
+    "SCIP",
+    "HiGHS"
+]
 
+# Initialize a dictionary to track any problems arising in the process
 problems = Dict()
 
-# Set up command-line parser
-s = ArgParseSettings()
-@add_arg_table s begin
-    "-c"
-        help = "make clean, i.e. delete existing *.toml files in current directory"
-        action = :store_true
-end
-CLI_args = parse_args(s)
-
-# If the user specifies the -c flag, try to delete Manifest.toml and
-#   Project.toml, if they exist in the current directory
-if CLI_args["c"] == true
-    files_to_delete = ["Manifest.toml", "Project.toml"]
-    for dfile in files_to_delete
-        try
-            rm(abspath(dfile))
-            @info "Removed file $dfile"
-        catch e
-            if occursin("no such file", e.msg)
-                current_dir = @__DIR__
-                @info "No extant file $dfile in the current directory, $current_dir."
-            else
-                throw(e)
-            end
+# Delete preexisting .toml files to avoid contamination
+files_to_delete = ["Manifest.toml", "Project.toml"]
+for dfile in files_to_delete
+    try
+        rm(abspath(dfile))
+        @info "Removed file $dfile"
+    catch e
+        if occursin("no such file", e.msg)
+            current_dir = @__DIR__
+            @info "No extant file $dfile in the current directory, $current_dir."
+        else
+            throw(e)
         end
     end
 end
@@ -60,20 +52,21 @@ end
 # Activate local environment
 Pkg.activate(".")
 
-# Add Julia libraries needed for ALEAF
-# Add and build the optimizer packages
-try
-    println("Adding CPLEX")
-    Pkg.add("CPLEX")
-    println("Building CPLEX...")
-    Pkg.build("CPLEX")
-    println("CPLEX built.")
-catch e
-    @warn "There was a problem installing or building CPLEX."
-    problems["CPLEX"] = e
-end
+# Try to add CPLEX to the environment
+# CPLEX may not be available on all platforms. If CPLEX is unavailable, 
+#   alert the user and proceed without it.
+#try
+#    println("Adding CPLEX")
+#    Pkg.add("CPLEX")
+#    println("Building CPLEX...")
+#    Pkg.build("CPLEX")
+#    println("CPLEX built.")
+#catch e
+#    @warn "There was a problem installing or building CPLEX."
+#    problems["CPLEX"] = e
+#end
 
-# Add the non-optimizer packages (which don't need to be built)
+# Add all Julia packages to the environment, and ensure they are all built
 for i=1:size(julia_pkg_list)[1]
     pkg = julia_pkg_list[i]
     try
