2e39613f9aa8cb5945c24c8d52f10b539b25cbe5
kbiegel <kbiegel@anl.gov>
2022-10-20 10:43:32 -0500

    adding tests of read_settings() in run.py

---
+++ test/test_run.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.path.append("../")
+
+from run import *
+
+# Files and parameters
+settings_file_path = "./testing_settings.yml"
+all_keys = ["simulation", "scenario", "constants", "file_paths", "system", "demand", "dispatch", "agent_opt", "ALEAF"]
+
+
+# Test the read_settings() function
+settings = read_settings(settings_file_path)
+
+def test_read_settings_has_all_keys():
+    assert all(key in all_keys for key in settings.keys())
+
+def test_read_settings_no_loose_keys():
+    assert all(key in settings.keys() for key in all_keys)
+
+def test_read_settings_scenario_name():
+    assert settings["simulation"]["ALEAF_scenario_name"] == "test_scenario"
+
+def test_read_settings_peak_demand():
+    assert settings["scenario"]["peak_demand"] == 29000
