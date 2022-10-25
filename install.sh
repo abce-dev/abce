3de54fb1602511ed0ea7c56426b00b9aab98308b
kbiegel <kbiegel@anl.gov>
2022-10-25 10:02:28 -0500

    fixing .bashrc updates to work properly; removing all attempts to set environment variables in the current terminal session

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -i
 
 #################################################################
 # Install script initialization
@@ -21,14 +21,12 @@ do
 done
 
 #################################################################
-# Set up environment variables
+# Set values for environment variables
 #################################################################
 
 # Set ABCE_DIR to the location of this script
-ABCE_DIR=$( dirname -- $( readlink -f -- "$0"; ) )
-
-echo "Setting environment variable ABCE_DIR=${ABCE_DIR}"
-export ABCE_DIR="${ABCE_DIR}"
+abce_dir=$( dirname -- $( readlink -f -- "$0"; ) )
+echo "\$ABCE_DIR will be set to $abce_dir"
 
 # Set up ALEAF_DIR
 # If specified by a command-line argument, don't prompt the user for the
@@ -40,11 +38,34 @@ if [[ ! -n ${aleaf_dir+x} ]]; then
     echo "If you do not have ALEAF installed, press Enter to leave this variable empty."
     read aleaf_dir
 fi
+echo "\$ALEAF_DIR will be set to $aleaf_dir"
 
-echo "Setting environment variable ALEAF_DIR=$aleaf_dir"
-export ALEAF_DIR=$aleaf_dir
+#################################################################
+# Install Julia 1.8, if not already found
+#################################################################
 
+# If julia 1.8 is not the current version of julia, download and install
+#   julia-1.8.2
+updated_julia=0
+if [[ -z $( julia --version | grep "1.8" ) ]]; then
+    # 
+    if [[ ! -d "$HOME/julia-1.8.2" ]]; then
+        if [[ ! -f "$HOME/julia-1.8.2-linux-x86_64.tar.gz" ]]; then
+            echo "Downloading Julia 1.8.2...";
+            wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.2-linux-x86_64.tar.gz -P "$HOME";
+        fi
+
+        echo "Installing Julia 1.8.2..."
+        tar zxvf "$HOME/julia-1.8.2-linux-x86_64.tar.gz";
+    fi
+    updated_julia=1
+fi
+
+
+#################################################################
 # Update the .bashrc file
+#################################################################
+
 # If the .bashrc file doesn't already exist: create it
 echo "${RC_FILE}"
 if [[ ! -f "${RC_FILE}" ]]; then
@@ -54,22 +75,43 @@ fi
 
 # Create an associative array to allow looping over ABCE environment variables
 declare -A env_vars
-env_vars=( ["ABCE_DIR"]="$ABCE_DIR" ["ALEAF_DIR"]="$ALEAF_DIR" )
+env_vars=( ["ABCE_DIR"]="$abce_dir" ["ALEAF_DIR"]="$aleaf_dir" )
 
 # Update or append values for each environment variable in the rc file
-echo "Updating environment variables in the \$HOME/.bashrc file"
+echo "Updating environment variables in ${RC_FILE}"
 for var_name in "${!env_vars[@]}";
 do
     if grep -q "${var_name}" "${RC_FILE}"; then
     # If the form "export $var_name" is found at the start of a line,
     #   replace the rest of the line with the appropriate value
+        echo "Found ${var_name}; updating its referenced path";
+        grep -E "^export $var_name=.*" "${RC_FILE}";
         sed -i "s|^export $var_name=.*|export $var_name=${env_vars[$var_name]}|" "${RC_FILE}";
     else
     # If the form "export $var_name" does not start any lines in the rc file,
     #   append a line at the end of the rc file to export this variable
+        echo "Did not find ${var_name}; adding a new line to .bashrc";
         echo "export ${var_name}=${env_vars[$var_name]}" >> "${RC_FILE}"
     fi
 done
+
+# If this script installed Julia 1.8.2, make sure it's added appropriately
+#   to the PATH
+if [[ $updated_julia ]]; then
+    echo "Ensuring Julia 1.8.2 is added to \$PATH in ${RC_FILE}";
+    if grep -q "export PATH=.*julia.*" "${RC_FILE}"; then
+        # If .bashrc already has a line adding julia to the path, update it to 1.8.2
+        echo "Updating existing julia reference in ${RC_FILE}";
+        sed -i "s|^export PATH=.*julia-.*|export PATH=\$PATH:$HOME/julia-1.8.2/bin/|" "${RC_FILE}";
+    else
+        # Otherwise, add a new line adding julia to the path
+        echo "Adding julia to \$PATH";
+        echo "export PATH=\$PATH:$HOME/julia-1.8.2/bin/" >> "${RC_FILE}";
+    fi
+fi
+
+
+<< EOF
 
 #################################################################
 # Set up the Python environment
@@ -107,6 +149,7 @@ echo "Python environment created successfully."
 # Set up the Julia environment
 #################################################################
 
+
 echo "Setting up the local Julia environment..."
 julia make_julia_environment.jl --clean
 
@@ -119,5 +162,8 @@ echo "Julia environment created successfully."
 # Prevent current value of $aleaf_dir from contaminating future runs of this
 #   script in the same terminal session
 unset aleaf_dir
+unset JULIA_DIR
 
 echo "All setup completed successfully."
+
+EOF
