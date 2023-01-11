23f9d397dff4207d7c21b599bd1b0eaf2434db29
kbiegel <kbiegel@anl.gov>
2023-01-11 10:13:07 -0600

    change solver_setting_list from an actual list into a string list

--- template_ALEAF.py
+++ template_ALEAF.py
@@ -19,6 +19,9 @@ def create_ALEAF_Master_file(ALEAF_data, settings):
     # Create an ExcelWriter object to contain all tabs and save file
     writer = pd.ExcelWriter("testALEAF_Master.xlsx", engine="openpyxl")
 
+    # Dictionary of all tabs and their metadata
+    # In final implementation, will be replaced by the ALEAF standard
+    #   data schema
     tabs_to_create = {
         "ALEAF Master Setup": {
             "ABCE_tab_name": "ALEAF_Master_setup",
@@ -30,6 +33,7 @@ def create_ALEAF_Master_file(ALEAF_data, settings):
         }
     }
 
+    # Load all tab data into pandas dataframes
     for ALEAF_tab_name, tab_data in tabs_to_create.items():
         df = (pd.DataFrame.from_dict(
                   ALEAF_data["ALEAF_Master"][tab_data["ABCE_tab_name"]],
@@ -40,7 +44,7 @@ def create_ALEAF_Master_file(ALEAF_data, settings):
         tabs_to_create[ALEAF_tab_name]["data"] = df
 
     # Finalize the CPLEX Setting tab data
-    solver_setting_list = [parameter for parameter in tabs_to_create["CPLEX Setting"]["data"].iloc[:, 0]]
+    solver_setting_list = ", ".join([parameter for parameter in tabs_to_create["CPLEX Setting"]["data"].iloc[:, 0]])
     CPLEX_extra_rows = {
         "solver_direct_mode_flag": "TRUE",
         "num_solver_setting": len(solver_setting_list),
