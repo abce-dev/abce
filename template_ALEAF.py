a279d779ae0c82e975e49c73b17c5e42f2f880ae
kbiegel <kbiegel@anl.gov>
2023-01-11 09:57:28 -0600

    extending tab creation and writing to apply to a dictionary of tabs

--- template_ALEAF.py
+++ template_ALEAF.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yaml
+import openpyxl
 
 ALEAF_data_file = "./inputs/ALEAF_settings.yml"
 unit_specs_data_file = "./inputs/unit_specs.yml"
@@ -15,16 +16,39 @@ def load_data(file_name):
 
 
 def create_ALEAF_Master_file(ALEAF_data, settings):
-    # Create the "ALEAF Master Setup" tab
-    ALEAF_Master_setup = (pd.DataFrame.from_dict(
-                              ALEAF_data["ALEAF_Master"]["ALEAF_Master_setup"],
-                              orient="index",
-                              columns=["Value"]
-                          ).reset_index()
-                           .rename(columns={"index": "Setting"})
-                         )
-    print(ALEAF_Master_setup)
+    # Create an ExcelWriter object to contain all tabs and save file
+    writer = pd.ExcelWriter("ALEAF_Master.xlsx", engine="openpyxl")
 
+    tabs_to_create = {
+        "ALEAF Master Setup": {
+            "ABCE_tab_name": "ALEAF_Master_setup",
+            "data": None
+        },
+        "CPLEX Setting": {
+            "ABCE_tab_name": "CPLEX_settings",
+            "data": None
+        }
+    }
+
+    for ALEAF_tab_name, tab_data in tabs_to_create.items():
+        df = (pd.DataFrame.from_dict(
+                  ALEAF_data["ALEAF_Master"][tab_data["ABCE_tab_name"]],
+                  orient="index",
+                  columns=["Value"]
+              ).reset_index().rename(columns={"index": "Setting"}))
+
+        tabs_to_create[ALEAF_tab_name]["data"] = df
+
+    # Write all tabs to file
+    for ALEAF_tab_name, tab_data in tabs_to_create.items():
+        tab_data["data"].to_excel(
+            writer,
+            sheet_name = ALEAF_tab_name,
+            index=False
+        )
+
+    # Write all changes and close the file writer
+    writer.close()
 
 
 def create_ALEAF_files():
