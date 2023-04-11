2d48e3e25ae4dc4a29fe5e8d9f15aa8d3ab5125c
kbiegel <kbiegel@anl.gov>
2023-04-11 16:52:57 -0500

    splitting test module defintion into its own file

---
+++ test/test.jl
@@ -0,0 +1,72 @@
+module sTest
+
+include("../src/dispatch.jl")
+
+using Logging, DataFrames, YAML, .Dispatch
+
+function test(thing, value)
+    # Gather some information about the function calling test, for reporting
+    st = StackTraces.stacktrace()
+    outer_func_line, func_name, func_line = get_stacktrace_entries(st)
+
+    # Set up the variables for reporting test results
+    pass = true
+    test_header_msg = string(
+                          "Invoking test ", func_name,
+                          " (line ", outer_func_line, 
+                          "):\n  Sub-test at line ", func_line, 
+                          "\n  Result: "
+                      )
+    result_msg = "pass"
+    extra_info = ""
+
+    if typeof(value) in [DataType, UnionAll]
+    # If `value` is a DataType, perform type-checking for type or supertype
+        if !(typeof(thing) == value) && !(thing <: value)
+            pass = false
+            result_msg = "fail"
+            extra_info = string("/n", thing, " is not ", value)
+        end
+    else
+    # If `value` is not a DataType, assume a value comparison is desired
+        if thing != value
+            pass = false
+            result_msg = "fail"
+            extra_info = string("\n", thing, " != ", value)
+        end
+    end
+
+    msg = string(test_header_msg, result_msg, extra_info)
+
+    if pass
+        @info msg
+    else
+        @warn msg
+    end
+
+end
+
+
+function get_stacktrace_entries(st)
+    # Determine the location of the test invocation in the top-level scope
+    outer_func_line = st[3].line
+
+    # Determine the test being called and the location of the sub-call
+    #   to `test`
+    func_name = st[2].func
+    func_line = st[2].line
+
+    return outer_func_line, func_name, func_line
+end
+
+
+function report_results()
+    # Report summary of results to user
+    println("\n\n")
+    @info "==============SUMMARY=============="
+    @info "Total number of tests passed: $num_passed"
+    @info "Total number of tests failed: $num_failed"
+end
+
+
+end
--- test/test_julia.jl
+++ test/test_julia.jl
@@ -1,73 +1,7 @@
+include("./test.jl")
 include("../src/dispatch.jl")
 
-using Logging, DataFrames, YAML, .Dispatch
-
-function test(thing, value)
-    # Gather some information about the function calling test, for reporting
-    st = StackTraces.stacktrace()
-    outer_func_line, func_name, func_line = get_stacktrace_entries(st)
-
-    # Set up the variables for reporting test results
-    pass = true
-    test_header_msg = string(
-                          "Invoking test ", func_name,
-                          " (line ", outer_func_line, 
-                          "):\n  Sub-test at line ", func_line, 
-                          "\n  Result: "
-                      )
-    result_msg = "pass"
-
-    if typeof(value) in [DataType, UnionAll]
-    # If `value` is a DataType, perform type-checking for type or supertype
-        if !(typeof(thing) == value) && !(thing <: value)
-            pass = false
-            result_msg = string(thing, " is not ", value)
-        end
-    else
-    # If `value` is not a DataType, assume a value comparison is desired
-        if thing != value
-            pass = false
-            result_msg = string(thing, " != ", value)
-        end
-    end
-
-    msg = string(test_header_msg, result_msg)
-
-    if pass
-        global num_passed += 1
-        @info msg
-    else
-        global num_failed += 1
-        @warn msg
-    end
-end
-
-
-function get_stacktrace_entries(st)
-    # Determine the location of the test invocation in the top-level scope
-    outer_func_line = st[3].line
-
-    # Determine the test being called and the location of the sub-call
-    #   to `test`
-    func_name = st[2].func
-    func_line = st[2].line
-
-    return outer_func_line, func_name, func_line
-end
-
-
-function report_results()
-    # Report summary of results to user
-    println("\n\n")
-    @info "==============SUMMARY=============="
-    @info "Total number of tests passed: $num_passed"
-    @info "Total number of tests failed: $num_failed"
-end
-
-
-###########################################
-# User-defined tests here                 #
-###########################################
+using .sTest, .Dispatch, YAML, DataFrames
 
 function test_reshape_shadow_prices(shadow_prices, check_reshaped_shadow_prices, y, settings)
     reshaped_shadow_prices = Dispatch.reshape_shadow_prices(
@@ -76,10 +10,19 @@ function test_reshape_shadow_prices(shadow_prices, check_reshaped_shadow_prices,
                                  settings
                              )
 
-    test(reshaped_shadow_prices, check_reshaped_shadow_prices)
+    sTest.test(reshaped_shadow_prices, check_reshaped_shadow_prices)
 
     return reshaped_shadow_prices
 end
+
+
+function test_propagate_all_results(all_gc_results, all_prices, current_pd, end_year, test_gc_results, test_prices)
+    propagated_gc_results, propagated_prices = Dispatch.propagate_all_results(all_gc_results, all_prices, current_pd, end_year)
+
+    sTest.test(propagated_gc_results, test_gc_results)
+    sTest.test(propagated_prices, test_prices)
+end
+
 
 
 settings = YAML.load_file("../settings.yml")
@@ -87,38 +30,49 @@ test_shadow_prices = [-0.0 -0.0 -1.0 -5.0
                       -0.0 -2.0 -9.1 -10000.0
                       -1.5 -3.3 -0.0 -0.0]
 
-check = [1 1 1 0.0
-         1 1 2 0.0
-         1 1 3 1.0
-         1 1 4 5.0
-         1 2 1 0.0
-         1 2 2 2.0
-         1 2 3 9.1
-         1 2 4 9001
-         1 3 1 1.5
-         1 3 2 3.3
-         1 3 3 0.0
-         1 3 4 0.0]
+check_prices = [1 1 1 0.0
+                1 1 2 0.0
+                1 1 3 1.0
+                1 1 4 5.0
+                1 2 1 0.0
+                1 2 2 2.0
+                1 2 3 9.1
+                1 2 4 9001
+                1 3 1 1.5
+                1 3 2 3.3
+                1 3 3 0.0
+                1 3 4 0.0]
+check_prices = DataFrame(check_prices, [:y, :d, :h, :price])
 
-check = DataFrame(check, [:y, :d, :h, :price])
-check[!, :y] = convert.(Int64, check[:, :y])
-check[!, :d] = convert.(Int64, check[:, :d])
-check[!, :h] = convert.(Int64, check[:, :h])
+all_gc_results = [1 1 1 "abc" 1.0 1
+                  1 1 1 "def" 100 1
+                  1 1 2 "abc" 2.0 1
+                  1 1 2 "def" 112 2
+                  1 2 1 "abc" 75.1 1
+                  1 2 1 "def" 0.0 0
+                  1 2 2 "abc" 0.1 1
+                  1 2 2 "def" 10000 256]
+all_gc_results = DataFrame(all_gc_results, [:y, :d, :h, :unit_type, :gen, :commit])
+
+y2_gc_results = copy(all_gc_results)
+y2_gc_results[!, :y] .= 2
+
+c2 = copy(check_prices)
+c2[!, :y] .= 2
+
+check_prop_gc_results = vcat(all_gc_results, y2_gc_results)
+check_prop_prices = vcat(check_prices, c2)
+
 
 ###########################################
 # Test runner, with list of all tests     #
 ###########################################
 
 function run_tests()
-    # Set up global counters for passed and failed tests
-    global num_passed = 0
-    global num_failed = 0
-
     # Put list of tests here
-    test_reshape_shadow_prices(test_shadow_prices, check, 1, settings)
-    test_reshape_shadow_prices(test_shadow_prices, check, 2, settings)
+    all_prices = test_reshape_shadow_prices(test_shadow_prices, check_prices, 1, settings)
 
-    report_results()
+    test_propagate_all_results(all_gc_results, all_prices, 1, 2, check_prop_gc_results, check_prop_prices)
 end
 
 
