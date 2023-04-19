2720e76df0d00e905230031429669665ccc13390
kbiegel <kbiegel@anl.gov>
2023-04-19 14:24:59 -0500

    use testSysimage.so only if available

--- test/run_julia_tests.sh
+++ test/run_julia_tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-julia --project=../env -JtestSysimage.so test_julia.jl
+# Use testSysimage.so if available
+if [[ -f "$ABCE_DIR/test/testSysimage.so" ]]; then
+    julia --project="$ABCE_DIR/env" -J "$ABCE_DIR/test/testSysimage.so" "$ABCE_DIR/test/test_julia.jl"
+else
+    julia --project="$ABCE_DIR/env" "$ABCE_DIR/test/test_julia.jl"
+fi
