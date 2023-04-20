4c90860855d8f9a3ea056e3535640ca269ee9e51
kbiegel <kbiegel@anl.gov>
2023-04-20 09:41:11 -0500

    adding temporary stand-in julia tests file

--- test/run_julia_tests.sh
+++ test/run_julia_tests.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 test_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 abce_dir="$( dirname $test_dir )"
-cat "$abce_dir/env/Project.toml"
-echo $abce_dir
+
+echo "Running julia tests..."
+
 # Use testSysimage.so if available
 if [[ -f "$test_dir/testSysimage.so" ]]; then
     julia --project="$abce_dir/env" -J "$test_dir/testSysimage.so" "$test_dir/test_julia.jl"
 else
     julia --project="$abce_dir/env" "$test_dir/test_julia.jl"
 fi
+
+echo "Tests finished."
