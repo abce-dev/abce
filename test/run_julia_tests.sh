f236a4e042a1d132d2a212867d3390073ba70912
kbiegel <kbiegel@anl.gov>
2023-04-20 11:09:15 -0500

    adding cat

--- test/run_julia_tests.sh
+++ test/run_julia_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 test_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 abce_dir="$( dirname $test_dir )"
+cat "$abce_dir/env/Project.toml"
 echo $abce_dir
 # Use testSysimage.so if available
 if [[ -f "$test_dir/testSysimage.so" ]]; then
