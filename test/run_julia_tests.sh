84003880886766da21af96f3fed8327dc426e94a
kbiegel <kbiegel@anl.gov>
2023-05-05 16:14:56 -0500

    returning non-zero exit code if the tests return a non-zero code

--- test/run_julia_tests.sh
+++ test/run_julia_tests.sh
@@ -7,8 +7,12 @@ echo "Running julia tests..."
 # Use testSysimage.so if available
 if [[ -f "$test_dir/testSysimage.so" ]]; then
     julia --project="$abce_dir/env" -J "$test_dir/testSysimage.so" "$test_dir/test_julia.jl"
+    rc=$?
 else
     julia --project="$abce_dir/env" "$test_dir/test_julia.jl"
+    rc=$?
 fi
 
 echo "Tests finished."
+
+exit $rc
