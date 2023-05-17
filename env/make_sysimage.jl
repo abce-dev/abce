cc1081ce3f65683946d15f32f299ce731f461097
kbiegel <kbiegel@anl.gov>
2023-05-17 16:39:01 -0500

    adding or updating the Apache 2.0 license header in all src/, test/, and env/ source files

--- env/make_sysimage.jl
+++ env/make_sysimage.jl
@@ -1,5 +1,7 @@
 # Package and function precompiler for agent_choice.jl
 ##########################################################################
+# Copyright 2023 Argonne National Laboratory
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
