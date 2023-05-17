d612d060d872cc0f98ca472a9ea457df1802f9d9
kbiegel <kbiegel@anl.gov>
2023-05-17 16:43:33 -0500

    updating copyright dates to 2023

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 
 ##########################################################################
-# Copyright 2021 Argonne National Laboratory
+# Copyright 2023 Argonne National Laboratory
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
