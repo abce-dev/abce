eaf835bec5c60186330618ff9ee200eb83f211b4
kbiegel <kbiegel@anl.gov>
2023-09-06 12:31:27 -0500

    passing verbose_outputs to the necessary function in ABCEfunctions.jl

--- src/agent_choice.jl
+++ src/agent_choice.jl
@@ -187,6 +187,7 @@ function run_agent_choice()
         CLI_args["current_pd"],
         C2N_specs,
         dispatch_results,
+        CLI_args["verbose_outputs"],
     )
 
     # Update the agent's baseline projected financial statements, to use in
