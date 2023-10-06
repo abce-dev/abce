f3efb48564b64820cf9d365e4dc77d1833d9e1d7
kbiegel <kbiegel@anl.gov>
2023-10-06 15:07:35 -0500

    fixing off-by-one error in determination of coal units which are available for retirement

--- src/ABCEfunctions.jl
+++ src/ABCEfunctions.jl
@@ -1660,7 +1660,7 @@ function set_up_model(
     end
 
     for i = 1:size(coal_retirements)[2]
-        y = current_pd + i
+        y = current_pd + i - 1
         num_units = 0
 
         # Get number of planned coal units operating during this period
@@ -1916,7 +1916,7 @@ function record_asset_retirements(
                 db,
                 string(
                     "SELECT asset_id FROM assets WHERE unit_type = ? ",
-                    "AND retirement_pd = ? AND agent_id = ?",
+                    "AND retirement_pd = ? AND agent_id = ? AND C2N_reserved = 0",
                 ),
                 match_vals,
             ) |> DataFrame
